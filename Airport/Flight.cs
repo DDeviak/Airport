60b922f859d40ba16cb8cd3aee5f6f9f1694f379
DDeviak <volodybs@gmail.com>
2023-07-16 20:31:49 +0300

    Fixed Flight data Validation

--- Airport/Flight.cs
+++ Airport/Flight.cs
@@ -6,61 +6,89 @@ namespace Airport
     class Flight
     {
         [DataMember()]
-        public int ID { get; private set; }
+        public int ID { get; protected set; }
         [DataMember()]
-        private City departureCity;
+        protected City departureCity;
         public City DepartureCity {
             get { return departureCity; }
-            private set 
-            { 
+            set 
+            {
+                if (!Enum.IsDefined(typeof(City), value)) throw new ArgumentException("DepartureCity can`t be equal " + value);
                 if (value == arrivalCity) throw new ArgumentException("DepartureCity and ArrivalCity can`t be equal");
                 departureCity = value;
             }
         }
         [DataMember()]
-        private City arrivalCity;
+        protected City arrivalCity;
         public City ArrivalCity
         {
             get { return arrivalCity; }
-            private set
+            set
             {
+                if (!Enum.IsDefined(typeof(City), value)) throw new ArgumentException("ArrivalCity can`t be equal " + value);
                 if (departureCity == value) throw new ArgumentException("DepartureCity and ArrivalCity can`t be equal");
                 arrivalCity = value;
             }
         }
         [DataMember()]
-        public DateTime DepartureDatetime { get; private set; }
+        protected DateTime departureDatetime;
+        public DateTime DepartureDatetime
+        {
+            get { return departureDatetime; }
+            set
+            {
+                if (arrivalDatetime < value) throw new ArgumentException("DepartureDatetime can`t be later than ArrivalDatetime");
+                departureDatetime = value;
+            }
+        }
         [DataMember()]
-        public DateTime ArrivalDatetime { get; private set; }
+        protected DateTime arrivalDatetime;
+        public DateTime ArrivalDatetime
+        {
+            get { return departureDatetime; }
+            set
+            {
+                if (value < departureDatetime) throw new ArgumentException("ArrivalDatetime can`t be sooner than DepartureDatetime");
+                arrivalDatetime = value;
+            }
+        }
         [DataMember()]
-        public Airline Airline { get; private set; }
+        protected Airline airline;
+        public Airline Airline
+        {
+            get { return airline; }
+            set
+            {
+                if (!Enum.IsDefined(typeof(Airline), value)) throw new ArgumentException("Airline can`t be equal " + value);
+                airline = value;
+            }
+        }
         [DataMember()]
-        private double price;
+        protected double price;
         public double Price
         {
             get { return price; }
             set 
             {
                 if (value < 0) throw new ArgumentException("Price can`t be less than zero");
-                price = value; 
+                price = double.Round(value, 2);
             }
         }
 
         public Flight(int id, City departureCity, City arrivalCity, DateTime departureDatetime, DateTime arrivalDatetime, Airline airline, double price)
         {
-            if (arrivalDatetime < departureDatetime) throw new ArgumentException("ArrivalDatetime can`t be sooner than DepartureDatetime");
-            ID = id;
-            DepartureCity = departureCity;
-            ArrivalCity = arrivalCity;
-            DepartureDatetime = departureDatetime;
-            ArrivalDatetime = arrivalDatetime;
-            Airline = airline;
-            Price = price;
+            this.ID = id;
+            this.DepartureCity = departureCity;
+            this.ArrivalCity = arrivalCity;
+            this.departureDatetime = departureDatetime;
+            this.ArrivalDatetime = arrivalDatetime;
+            this.Airline = airline;
+            this.Price = price;
         }
 
         public override string ToString()
         {
-            return String.Format("{6}, {0} - {1}, {2} - {3}, {4}, ${5}", DepartureCity, ArrivalCity, DepartureDatetime, ArrivalDatetime, Airline, Price, ID);
+            return String.Format("{0}, {1} - {2}, {3} - {4}, {5}, ${6}", ID, DepartureCity, ArrivalCity, DepartureDatetime, ArrivalDatetime, Airline, Price);
         }
     }
 }
